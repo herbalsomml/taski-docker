6be32d4530633aa85e52294cb3534e5214a873c6
Егор Вирик <herbalsomml@yandex.ru>
2024-04-07 18:46:06 +0200

    Fixed main.yml

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,7 +12,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 DEBUG = strtobool(os.getenv('DEBUG', 'true'))
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split('')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost 127.0.0.1').split(' ')
 
 
 # Application definition
